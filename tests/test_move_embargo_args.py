21e3c8c9b410345639b1eabdb9da2ec1543795a1
beckynevin <rnevin@fnal.gov>
2023-03-17 19:22:27 +0000

    adding a comment for what we want to do with this test

--- tests/test_move_embargo_args.py
+++ tests/test_move_embargo_args.py
@@ -12,6 +12,7 @@ class TestSum(unittest.TestCase):
         args = [1, 2, 3] # need to insert arguments that go into move_embargo_args here to test it
         result = move_embargo_args(args)
         # I'm not sure what we need to assert (below)
+        # maybe the assertion can be related to missing arguments?
         self.assertEqual(result, 6)
 
 if __name__ == '__main__':
