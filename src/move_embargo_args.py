f6dd63803aa5248a73707b96decf4cb6d7e91b7e
Sreevani Jarugula <jsv1206@gmail.com>
2023-03-20 19:31:01 +0000

    comments

--- src/move_embargo_args.py
+++ src/move_embargo_args.py
@@ -3,10 +3,8 @@ parser = argparse.ArgumentParser(description='Transferring data from embargo but
 from lsst.daf.butler import Butler
 import astropy
 
-test_args = True
 remove_collection = False # function to remove a collection from scratch butler
 transfer = False
-main = False
 
 # Define all the arguments
 # at least one arg in dataId needed for 'where' clause.
@@ -32,53 +30,42 @@ scratch_registry = scratch_butler.registry
 datasetType = args.datasettype
 collections = args.collections
 
-if main:
-    # Dataset to move
-    if not [x for x in (args.instrument, args.detector, args.band) if x is None]: 
-        dataId = {'instrument': args.instrument, 'detector': args.detector, 'band':args.band}
-    elif not [x for x in (args.instrument, args.detector, args.band, args.exposure) if x is None]: 
-        dataId = {'instrument': args.instrument, 'detector': args.detector, 'band':args.band, 'exposure':args.exposure}
-    else:
-        dataId={'instrument': args.instrument}
+# Dataset to move
+# dataID must include
+if not [x for x in (args.instrument, args.detector, args.band) if x is None]: 
+    dataId = {'instrument': args.instrument, 'detector': args.detector, 'band':args.band}
+elif not [x for x in (args.instrument, args.detector, args.band, args.exposure) if x is None]: 
+    dataId = {'instrument': args.instrument, 'detector': args.detector, 'band':args.band, 'exposure':args.exposure}
+else:
+    dataId={'instrument': args.instrument}
 
-    # Define embargo period 
-    embargo_period = astropy.time.TimeDelta(args.embargodays, format='jd') # 30 julian days?
-    int_embargo_period = int(embargo_period.datetime.days)
-    now = astropy.time.Time.now()
-    int_now = int(now.datetime.strftime("%Y%m%d"))
+# Define embargo period 
+embargo_period = astropy.time.TimeDelta(args.embargodays, format='jd') # 30 julian days?
+int_embargo_period = int(embargo_period.datetime.days)
+now = astropy.time.Time.now()
+int_now = int(now.datetime.strftime("%Y%m%d"))
 
-    # The Dimensions query 
+# The Dimensions query 
+# If now - observation_end_time_in_embargo > embargo period : move   Else: don't move
+# Save data Ids of these observations into a list
+after_embargo = []
 
-    # If now - observation_end_time_in_embargo > embargo period : move   
-    # Else: don't move
+for i, dt in enumerate(registry.queryDimensionRecords('exposure',dataId=dataId,datasets=datasetType,collections=collections,
+                                                     where="now - exposure.day_obs > embargo_period",
+                                                     bind={"now": int_now, "embargo_period": int_embargo_period})):
+    end_time = dt.timespan.end
+    if now - end_time > embargo_period:
+        after_embargo.append(dt.id)
 
-    # Save data Ids of these observations into a list
-    after_embargo = []
-
-    for i, dt in enumerate(registry.queryDimensionRecords('exposure',dataId=dataId,datasets=datasetType,collections=collections,
-                                                         where="now - exposure.day_obs > embargo_period",
-                                                         bind={"now": int_now, "embargo_period": int_embargo_period})):
-        end_time = dt.timespan.end
-        if now - end_time > embargo_period:
-            after_embargo.append(dt.id)
-
-    # Query the DataIds after embargo period
-    datasetRefs = registry.queryDatasets(datasetType, dataId=dataId, collections=collections,
-                                             where="exposure.id IN (exposure_ids)",
-                                             bind={"exposure_ids": after_embargo})
+# Query the DataIds after embargo period
+datasetRefs = registry.queryDatasets(datasetType, dataId=dataId, collections=collections,
+                                         where="exposure.id IN (exposure_ids)",
+                                         bind={"exposure_ids": after_embargo})
 
 # Copy the Dataset after embargo period from embargo butler to scratch butler
 if transfer:
     scratch_butler.transfer_from(butler, source_refs=datasetRefs, transfer='copy',skip_missing=True,register_dataset_types=True,transfer_dimensions=True)
-
-if test_args:
-    for dt in sorted(registry.queryDatasetTypes(datasetType+'*')):
-        print(dt)
-    print('-----------------------')
-    for dt in sorted(scratch_registry.queryDatasetTypes(datasetType+'*')):
-        print(dt)
-    print('-----------------------')
-    print('embargo period: ',args.embargodays)
     
+# Remove collection from scratch butler   
 if remove_collection:
     scratch_butler.pruneCollection(collections, purge=True, unstore=True)
