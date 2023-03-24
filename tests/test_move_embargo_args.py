f6a0832dd07893201fb64be35726a7e4c5ee5899
beckynevin <rnevin@fnal.gov>
2023-03-24 17:17:06 +0000

    this is broken but at least is testing the parser arg

--- tests/test_move_embargo_args.py
+++ tests/test_move_embargo_args.py
@@ -1,18 +1,29 @@
 import unittest
-
 import sys
 sys.path.insert(0,"..")
 from src import move_embargo_args
 
-class TestSum(unittest.TestCase):
-    def test_list_int(self):
+class TestEmbargoArgs(unittest.TestCase):
+    def test_parser(self):
         """
-        Test something about move_embargo_args
+        Test the parser without having to execute the code
         """
-        args = [1, 2, 3] # need to insert arguments that go into move_embargo_args here to test it
-        # I'm not sure what we need to assert (below)
-        # maybe the assertion can be related to missing arguments?
-        self.assertRaises(move_embargo_args(args))
+        
+        
+        
+        args = ['-fromrepo', '/repo/embargo', '-torepo', '/home/j/jarugula/scratch', '-instrument', 'LATISS', '-days', '30', '-dtype', 'raw', '-coll', 'LATISS/raw/all', '-band', 'g']
+        
+        parser = move_embargo_args.parse_args(args)
+        self.assertTrue(parser.long)
+        
+        '''
+
+        #python move_embargo_args.py --help
+        #python move_embargo_args.py -fromrepo $arg_fromrepo -torepo $arg_torepo -instrument $arg_instrument -days $arg_days -dtype $arg_dtype
+        self.assertRaises(move_embargo_args())
+        
+        self.assertRaises(move_embargo_args("-fromrepo" = arg_fromrepo, "-torepo" = arg_torepo, "-instrument" = arg_instrument, "-days" = arg_days, "-dtype" = arg_dtype))
+        '''
 
 if __name__ == '__main__':
     unittest.main()
