62ab657a7c9db0e39be2c10f12267bc96f41b967
beckynevin <rnevin@fnal.gov>
2023-04-06 15:16:42 +0000

    shell script that creates a new fake butler
    two fake butlers now

---
+++ tests/create_fake_butler.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Shell script for creating a fake butler that you can use
+# for testing.
+# you also need the lsst_distrib loaded to run this file
+# in order to run this file:
+# chmod u+x create_fake_butler.sh
+# and then ./create_fake_butler.sh
+
+setup lsst_distrib
+#run this if you need to create scratch butler
+butler create fake_from
+
+butler create fake_to
+
