c7603db8013da0adf9edc1147b653403310080e0
beckynevin <rnevin@fnal.gov>
2023-04-18 20:28:31 +0000

    variables changed to be relative to input time window
    breaking on the where clause
    
    stuck on the pope on line 42
    
    this runs and moves stuff over
    
    runs through
    
    got this to work using subprocess
    
    fixed to work with subprocess
    
    removing raw_args argument
    
    prepping with to do items for cowork

--- tests/populate_test_butler.py
+++ tests/populate_test_butler.py
@@ -1,76 +1,72 @@
+import argparse
+import astropy.time
 from lsst.daf.butler import Butler
 import move_embargo_args
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='Transferring data into fake_from butler, for use in testing')
+    # at least one arg in dataId needed for 'where' clause.
+    parser.add_argument(
+        "-f", "--fromrepo", type=str,
+        required=True, default='/repo/embargo',
+        help="Butler Repository path from which data is transferred. Input str. Default = '/repo/embargo'")
+    parser.add_argument(
+        "-t", "--torepo", type=str,
+        required=True, help="Repository to which data is transferred. Input str")
+    parser.add_argument("-m", "--move_times", type=str, required=True,
+                        help="List of times you want to move", nargs="*")
+    parser.add_argument("-d", "--window_days", type=int, required=True, default=35,
+                        help="Time window around each time list entry you want to move")
+    return parser.parse_args()
 
-
-def populate_fake_butler(time):
+def populate_fake_butler(from_repo, to_repo, time, window_days, verbose = False):
     """
-    Uses our code (maybe some other code) to move files to our fake butler
-    from '/repo/embargo'
+    Moves files to our fake butler (fake_from) from '/repo/embargo'
     
-    We could create a new code that
     """
     # Probably will have to run the main program in 
     
-    butler = Butler(namespace.fromrepo)
+    butler = Butler(from_repo)
     registry = butler.registry
-    dest = Butler(namespace.torepo, writeable=True)
+    dest = Butler(to_repo, writeable=True)
     scratch_registry = dest.registry
-    datasetType = namespace.datasettype
-    collections = namespace.collections
+    datasetType = 'raw'
+    collections = 'LATISS/raw/all'
+    instrument = 'LATISS'
+    band = 'g'
+    dataId = {'instrument': instrument}
 
-    # Dataset to move
-    # dataID must include
-    if not [x for x in (namespace.instrument, namespace.detector, namespace.band) if x is None]:
-        dataId = {'instrument': namespace.instrument, 'detector': namespace.detector,
-                  'band': namespace.band}
-    elif not [x for x in (namespace.instrument,
-                          namespace.detector,
-                          namespace.band,
-                          namespace.exposure) if x is None]:
-        dataId = {'instrument': namespace.instrument, 'detector': namespace.detector,
-                  'band': namespace.band, 'exposure': namespace.exposure}
-    else:
-        dataId = {'instrument': namespace.instrument}
-
-    # Define embargo period
-    embargo_period = astropy.time.TimeDelta(namespace.embargodays, format='jd')
-    now = astropy.time.Time.now()
-    int_now = int(now.datetime.strftime("%Y%m%d"))
-
-    # The Dimensions query
-    # If now - observation_end_time_in_embargo > embargo period : move
-    # Else: don't move
-    # Save data Ids of these observations into a list
-    after_embargo = []
-
+    # Define time window to be slightly larger than the embargo period of 30 days
+    window = astropy.time.TimeDelta(window_days, format='jd')
+    time_astropy = astropy.time.Time(time)
+    int_time = int(time_astropy.datetime.strftime("%Y%m%d"))
+    within_window = []
+    times = []
     for i, dt in enumerate(registry.queryDimensionRecords('exposure', dataId=dataId, datasets=datasetType,
                                                           collections=collections,
-                                                          where="now - exposure.day_obs > embargo_period",
-                                                          bind={"now": int_now,
-                                                                "embargo_period": namespace.embargodays})):
+                                                          where="now - window < exposure.day_obs and now + window > exposure.day_obs",
+                                                          bind={"now": int_time,
+                                                                "window": window_days})):
         end_time = dt.timespan.end
-        if now - end_time > embargo_period:
-            after_embargo.append(dt.id)
-
+        if (time_astropy - window < end_time) and (time_astropy + window > end_time):
+            within_window.append(dt.id)
+            times.append(end_time)
+    if verbose:
+        print(f'moving {len(within_window)} data refs')
     # Query the DataIds after embargo period
     datasetRefs = registry.queryDatasets(datasetType, dataId=dataId, collections=collections,
                                          where="exposure.id IN (exposure_ids)",
-                                         bind={"exposure_ids": after_embargo})
-
-    # Copy the Dataset after embargo period from
-    # embargo butler to scratch butler.
-    if TRANSFER:
-        dest.transfer_from(butler, source_refs=datasetRefs, transfer='copy',
+                                         bind={"exposure_ids": within_window})
+    dest.transfer_from(butler, source_refs=datasetRefs, transfer='copy',
                            skip_missing=True, register_dataset_types=True,
                            transfer_dimensions=True)
-        
-        
+
+def main():
+    namespace = parse_args()
+    for time in namespace.move_times:
+        populate_fake_butler(namespace.fromrepo, namespace.torepo, time, namespace.window_days, verbose = True)
 
 
 if __name__ == '__main__':
-    # do the thing
-    time_list = [time_1, time_2]
-    for time in time_list:
-        
-        populate_fake_butler(time)
+    main()
+
--- tests/test_contents_fake_butler.py
+++ tests/test_contents_fake_butler.py
@@ -1,5 +1,8 @@
 import unittest
 from lsst.daf.butler import Butler
+import move_embargo_args
+import populate_test_butler
+import subprocess
 
 
 class TestContents(unittest.TestCase):
@@ -7,35 +10,22 @@ class TestContents(unittest.TestCase):
         """
         Test the times of the files in the fake_to butler
         """
-        # Probably will have to run the main program in 
-        # order to do the moving, using a static 'now' time
-        '''
-        args = ['-f', '/repo/embargo',
-                '-t', '/home/j/jarugula/scratch',
-                '--instrument', 'LATISS',
-                '-days', '30',
-                '--datasettype', 'raw',
-                '--collections', 'LATISS/raw/all',
-                '--band', 'g']
-        parser = move_embargo_args.parser(args)
-        self.assertTrue(parser.long)
-        '''
-        # Is there an 'is in' assert? That might be nice
-        namespace = 'fake_to'
-        butler = Butler(namespace)
+        # First step is to populate the fake test butler
+        # I've only gotten this to work using subprocess
+        subprocess.call(['python', 'populate_test_butler.py', '-f','/repo/embargo','-t','fake_from','-m','[2022-01-31T,2022-04-18]','-d', '35'])
+        butler = Butler('/repo/embargo')
         registry = butler.registry
-        
-        print('running through')
-        
         for i, dt in enumerate(registry.queryDimensionRecords('exposure')):
             end_time = dt.timespan.end
-            print(end_time)
-            
+            print(end_time)     
         print('done printing times')
-        #print(list_butler)
         
+        # Run the move_embargo_args code
+        # with transfer='move' as an option in move_embargo_args.py
+        # remove the where clause from move_embargo_args.py and current code
         
-
+        # Look through the times of everything 
+        
 
 if __name__ == '__main__':
     unittest.main()
