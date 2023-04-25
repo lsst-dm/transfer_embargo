2374b97cac59f34ebfda1792cd772c6759611997
beckynevin <rnevin@fnal.gov>
2023-04-25 19:16:18 +0000

    prepping with to do items for cowork

--- tests/test_contents_fake_butler.py
+++ tests/test_contents_fake_butler.py
@@ -12,13 +12,20 @@ class TestContents(unittest.TestCase):
         """
         # First step is to populate the fake test butler
         # I've only gotten this to work using subprocess
-        subprocess.call(['python', 'populate_test_butler.py', '-f','/repo/embargo','-t','fake_from','-m','[2022-01-31,2022-04-18]','-d', '35'])
+        subprocess.call(['python', 'populate_test_butler.py', '-f','/repo/embargo','-t','fake_from','-m','[2022-01-31T,2022-04-18]','-d', '35'])
         butler = Butler('/repo/embargo')
         registry = butler.registry
         for i, dt in enumerate(registry.queryDimensionRecords('exposure')):
             end_time = dt.timespan.end
             print(end_time)     
         print('done printing times')
+        
+        # Run the move_embargo_args code
+        # with transfer='move' as an option in move_embargo_args.py
+        # remove the where clause from move_embargo_args.py and current code
+        
+        # Look through the times of everything 
+        
 
 if __name__ == '__main__':
     unittest.main()
