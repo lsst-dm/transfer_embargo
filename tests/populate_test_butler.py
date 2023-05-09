14ff951b3700ebb82ff42e8859669c33b58b8657
beckynevin <rnevin@fnal.gov>
2023-05-09 16:59:44 +0000

    removing logging and tracking num transfered

--- tests/populate_test_butler.py
+++ tests/populate_test_butler.py
@@ -1,6 +1,7 @@
 import argparse
 import astropy.time
 from lsst.daf.butler import Butler, Timespan
+import numpy as np
 
 
 def parse_args():
@@ -53,15 +54,11 @@ def populate_fake_butler(from_repo, to_repo, time, window_days, verbose=False):
                                          bind={"exposure_ids": within_window})
     if verbose:
         print(f'beginning the move from {from_repo} to {to_repo}')
-    # FIX
-    import logging
-    obj = logging.getLogger('lsst.daf.butler').setLevel('DEBUG')
-    
-    dest.transfer_from(butler, source_refs=datasetRefs, transfer='copy',
+    out = dest.transfer_from(butler, source_refs=datasetRefs, transfer='copy',
                        skip_missing=True, register_dataset_types=True,
                        transfer_dimensions=True)
     if verbose:
-        print('all files have supposedly been moved')
+        print(f'{np.shape(out)} files have been moved')
 
 
 def main():
