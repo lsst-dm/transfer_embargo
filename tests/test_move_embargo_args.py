5979a726a84cdda370bce833f9ec071d2d268c98
Sreevani Jarugula <jsv1206@gmail.com>
2023-05-22 20:14:48 +0000

    test_from and test_to in setup

--- tests/test_move_embargo_args.py
+++ tests/test_move_embargo_args.py
@@ -12,11 +12,10 @@ def is_it_there(
     now_time_embargo: str,
     ids_remain,
     ids_moved,
-    test_from, 
+    test_from,
     test_to,
     move,
 ):
-
     # Run the package
     subprocess.call(
         [
@@ -57,7 +56,7 @@ def is_it_there(
         for dt in registry_to.queryDatasets(datasetType=..., collections=...)
     ]
     print(id_in_to)
-        
+
     for ID in ids_moved:
         assert ID in id_in_to, f"{ID} should be in {test_to} repo but isnt :("
     for ID in id_in_to:
@@ -71,12 +70,14 @@ def is_it_there(
         for dt in registry_from.queryDatasets(datasetType=..., collections=...)
     ]
     print(id_in_from)
-    
+
     if move == "True":
         for ID in id_in_from:
             assert ID in ids_remain, f"{ID} should not be in {test_from} repo but it is"
         for ID in ids_remain:
-            assert ID in id_in_from + id_in_to, f"{ID} should not be in {test_from} repo but it is"
+            assert (
+                ID in id_in_from + id_in_to
+            ), f"{ID} should not be in {test_from} repo but it is"
     else:
         for ID in id_in_from:
             assert ID in ids_remain, f"{ID} should be in {test_from} repo but it isn't"
@@ -84,17 +85,17 @@ def is_it_there(
             assert ID in id_in_from, f"{ID} should be in {test_from} repo but it isn't"
 
 
-class TestMoveEmbargoArgs(unittest.TestCase):     
+class TestMoveEmbargoArgs(unittest.TestCase):
     def setUp(self):
         temp_dir = tempfile.TemporaryDirectory()
-        temp_from_path = os.path.join(temp_dir.name, 'temp_test_from')
-        temp_to_path = os.path.join(temp_dir.name, 'temp_test_to')
+        temp_from_path = os.path.join(temp_dir.name, "temp_test_from")
+        temp_to_path = os.path.join(temp_dir.name, "temp_test_to")
         shutil.copytree("data/test_from", temp_from_path)
         shutil.copytree("data/test_to", temp_to_path)
         self.temp_dir = temp_dir
         self.temp_from_path = temp_from_path
         self.temp_to_path = temp_to_path
-        
+
         now_time_embargo = "2020-03-01 23:59:59.999999"
         embargo_hours = 3827088.677299 / 3600  # hours
         # IDs that should be moved:
@@ -104,7 +105,7 @@ class TestMoveEmbargoArgs(unittest.TestCase):
             2020011700002,
             2020011700003,
             2020011700004,
-            ]
+        ]
         # IDs that should stay in the fake_from:
         ids_remain = [
             2019111300059,
@@ -114,23 +115,31 @@ class TestMoveEmbargoArgs(unittest.TestCase):
             2020011700004,
             2020011700005,
             2020011700006,
-            ]
-        
+        ]
+
         self.now_time_embargo = now_time_embargo
         self.embargo_hours = embargo_hours
         self.ids_moved = ids_moved
         self.ids_remain = ids_remain
-        
+
     def tearDown(self):
         shutil.rmtree(self.temp_dir.name, ignore_errors=True)
-        
+
     def test_main_move(self):
         """
         Run move_embargo_args to move some IDs from the fake_from butler
         to the fake_to butler and test which ones moved
         """
         move = "True"
-        is_it_there(self.embargo_hours, self.now_time_embargo, self.ids_remain, self.ids_moved, self.test_from, self.test_to, move=move)
+        is_it_there(
+            self.embargo_hours,
+            self.now_time_embargo,
+            self.ids_remain,
+            self.ids_moved,
+            self.temp_from_path,
+            self.temp_to_path,
+            move=move,
+        )
 
     def test_main_copy(self):
         """
@@ -138,7 +147,15 @@ class TestMoveEmbargoArgs(unittest.TestCase):
         to the fake_to butler and test which ones moved
         """
         move = "False"
-        is_it_there(self.embargo_hours, self.now_time_embargo, self.ids_remain, self.ids_moved, self.test_from, self.test_to, move=move)
+        is_it_there(
+            self.embargo_hours,
+            self.now_time_embargo,
+            self.ids_remain,
+            self.ids_moved,
+            self.temp_from_path,
+            self.temp_to_path,
+            move=move,
+        )
 
     # def test_time_format_input(self):
     #     test_from = self.temp_from_path
