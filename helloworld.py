7c52801028eb01e2f02b7a682e5d758c9a117e3d
Sreevani Jarugula <jsv1206@gmail.com>
2023-08-14 16:22:52 +0000

    output helloworld to text file

--- helloworld.py
+++ helloworld.py
@@ -6,3 +6,8 @@ def simple_function(x):
 
 print("hello world")
 print("return x+1: ", simple_function(2))
+
+with open('./helloworld_output.txt', 'w') as f:
+    f.write(simple_function(2))
+f.close()
+    
