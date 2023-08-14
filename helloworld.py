7c28125bfb0b6253a58769c3fedeb42e7b6793c9
Sreevani Jarugula <jsv1206@gmail.com>
2023-08-14 16:32:02 +0000

    unbuffered std output in dockerfile

--- helloworld.py
+++ helloworld.py
@@ -7,7 +7,7 @@ def simple_function(x):
 print("hello world")
 print("return x+1: ", simple_function(2))
 
-with open('./helloworld_output.txt', 'w') as f:
-    f.write(str(simple_function(2)))
-f.close()
+# with open('./helloworld_output.txt', 'w') as f:
+#     f.write(str(simple_function(2)))
+# f.close()
     
