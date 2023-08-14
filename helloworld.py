e11fd9eb1019a3803a3cdcbe8e1b3d98eb2d1fdc
Sreevani Jarugula <jsv1206@gmail.com>
2023-08-14 16:49:29 +0000

    adding env variables

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,28 @@
 # import numpy as np
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+            "--num",
+            type=int,
+            default=3,
+        )
+    return parser.parse_args()
 
 def simple_function(x):
     x = x+1
     return x
 
-print("hello world")
-print("return x+1: ", simple_function(2))
+# print("return x+1: ", simple_function(2))
+
+if __name__ == "__main__":
+    print("hello world")
+    namespace = parse_args()
+    print("input arg: ", namespace.num)
+    print("output from arg: ", simple_function(namespace.num))
+    
 
 # with open('./helloworld_output.txt', 'w') as f:
 #     f.write(str(simple_function(2)))
