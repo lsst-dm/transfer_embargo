9613dbe706e59865bbad16574eb375e831e10ab8
beckynevin <rnevin@fnal.gov>
2024-02-05 23:07:32 +0000

    idk why tests are failing but theres some sort of exposure error

--- tests/test_move_embargo_args.py
+++ tests/test_move_embargo_args.py
@@ -28,6 +28,9 @@ def is_it_there(
     iterable_datasettype = utils.iteration.ensure_iterable(datasettype)
     iterable_collections = utils.iteration.ensure_iterable(collections)
 
+    print("datasettype", datasettype)
+    print("after ensure iterable", iterable_datasettype)
+
     # Run the package
     subprocess.run(
         [
@@ -59,23 +62,26 @@ def is_it_there(
     registry_to = butler_to.registry
     counter = 0
     for dtype in datasettype:
-        if any(
-            dim in ["exposure", "visit"]
-            for dim in registry_to.queryDatasetTypes(dtype)[0].dimensions.names
-        ):
-            print(
-                "dtype with exposure or visit info: ", dtype
-            )
-            ids_in_temp_to = [
-                dt.dataId.mapping["exposure"]
-                for dt in registry_to.queryDatasets(datasetType=..., collections=...)
-            ]
-        else:
-            print("dtype with no exposure", dtype)
-            datasetRefs = registry_to.queryDatasets(
-                datasetType=datasettype, collections=collections
-            )
-            ids_in_temp_to = [dt.id for dt in datasetRefs]
+        try:
+            if any(
+                dim in ["exposure", "visit"]
+                for dim in registry_to.queryDatasetTypes(dtype)[0].dimensions.names
+            ):
+                print("dtype with exposure or visit info: ", dtype)
+                ids_in_temp_to = [
+                    dt.dataId.mapping["exposure"]
+                    for dt in registry_to.queryDatasets(
+                        datasetType=..., collections=...
+                    )
+                ]
+            else:
+                print("dtype with no exposure", dtype)
+                datasetRefs = registry_to.queryDatasets(
+                    datasetType=datasettype, collections=collections
+                )
+                ids_in_temp_to = [dt.id for dt in datasetRefs]
+        except IndexError:
+            ids_in_temp_to = []
 
         # verifying the contents of the temp_to butler
         # check that what we expect to move (ids_should_be_moved)
@@ -87,23 +93,29 @@ def is_it_there(
         # now check the temp_from butler and see what remains
         butler_from = Butler(temp_from)
         registry_from = butler_from.registry
-        
-        if any(
-            dim in ["exposure", "visit"]
-            for dim in registry_to.queryDatasetTypes(dtype)[0].dimensions.names
-        ):
-            ids_in_temp_from = [
-                dt.dataId.mapping["exposure"]
-                for dt in registry_from.queryDatasets(datasetType=..., collections=...)
-            ]
-        else:
-            ids_in_temp_from = [
-                dt.id
-                for dt in registry_from.queryDatasets(
-                    datasetType=..., collections=...
-                )
-            ]
-            
+
+        # first check if anything is in the registry_to:
+        try:
+            if any(
+                dim in ["exposure", "visit"]
+                for dim in registry_from.queryDatasetTypes(dtype)[0].dimensions
+            ):
+                ids_in_temp_from = [
+                    dt.dataId.mapping["exposure"]
+                    for dt in registry_from.queryDatasets(
+                        datasetType=..., collections=...
+                    )
+                ]
+            else:
+                ids_in_temp_from = [
+                    dt.id
+                    for dt in registry_from.queryDatasets(
+                        datasetType=..., collections=...
+                    )
+                ]
+        except IndexError:
+            ids_in_temp_from = []
+
         # verifying the contents of the from butler
         # if move is on, only the ids_remain should be in temp_from butler
         if move == "True":
@@ -122,11 +134,15 @@ def is_it_there(
             ), f"move is {move} and {ids_in_temp_from} should be in either \
                     {temp_from} or {temp_to} repo but it isn't"
         counter += 1
-    assert counter != 0, f"Never went through the for loop shame on you, counter = {counter}"
+    assert (
+        counter != 0
+    ), f"Never went through the for loop shame on you, counter = {counter}"
 
-class AtLeastOneAssertionFailed(Exception):
+
+class AtLeastOneAssertionFailedError(Exception):
     pass
-    
+
+
 class TestMoveEmbargoArgs(unittest.TestCase):
     def setUp(self):
         """
@@ -159,6 +175,150 @@ class TestMoveEmbargoArgs(unittest.TestCase):
         Removes all test files created by tests
         """
         shutil.rmtree(self.temp_dir.name, ignore_errors=True)
+
+    def test_calexp_should_move(self):
+        """
+        Test that move_embargo_args runs for
+        the calexp datatype
+        """
+        move = "False"
+        # now_time_embargo = "now"
+        # embargo_hours =  80.0 # hours
+        now_time_embargo = "2022-11-11 03:35:12.836981"
+        #"2020-01-17 16:55:11.322700"
+        embargo_hours = 80.0  # hours
+        # IDs that should be moved to temp_to:
+        ids_moved = [
+            2022110800580,
+            2022110900673,
+            2022111000436
+        ]
+        # IDs that should stay in the temp_from:
+        ids_remain = [
+            2022110800580,
+            2022110900673,
+            2022111000436
+        ]
+        is_it_there(
+            embargo_hours,
+            now_time_embargo,
+            ids_remain,
+            ids_moved,
+            self.temp_from_path,
+            self.temp_to_path,
+            move=move,
+            log=self.log,
+            datasettype=["calexp"],
+            collections=["LATISS/runs/AUXTEL_DRP_IMAGING_2022-11A/w_2022_46/PREOPS-1616"],
+            desturiprefix=self.temp_dest_ingest,
+            # desturiprefix="tests/data/",
+        ) 
+
+# test the other datatypes:
+    # first goodseeingdeepcoadd
+
+    def test_calexp_should_not_move(self):
+        """
+        Test that move_embargo_args does not move
+        the calexp data that is too close to embargo
+        """
+        move = "False"
+        # now_time_embargo = "now"
+        # embargo_hours =  80.0 # hours
+        now_time_embargo = "2022-11-11 03:35:12.836981"
+        #"2020-01-17 16:55:11.322700"
+        embargo_hours = 80.0  # hours
+        # IDs that should be moved to temp_to:
+        ids_moved = []
+        # IDs that should stay in the temp_from:
+        ids_remain = [
+            2022110800580,
+            2022110900673,
+            2022111000436
+        ]
+        is_it_there(
+            embargo_hours,
+            now_time_embargo,
+            ids_remain,
+            ids_moved,
+            self.temp_from_path,
+            self.temp_to_path,
+            move=move,
+            log=self.log,
+            datasettype=["calexp"],
+            collections=["LATISS/raw/all"],
+            desturiprefix=self.temp_dest_ingest,
+            # desturiprefix="tests/data/",
+        )
+'''
+    def test_after_now_01(self):
+        """
+        Verify that exposures after now are not being moved
+        when the nowtime is right in the middle of the exposures
+        """
+        move = "False"
+        now_time_embargo = "2020-01-17 16:55:11.322700"
+        embargo_hours = 0.1  # hours
+        # IDs that should be moved to temp_to:
+        ids_moved = [
+            2019111300059,
+            2019111300061,
+            2020011700002,
+            2020011700003,
+        ]
+        # IDs that should stay in the temp_from:
+        ids_remain = [
+            2020011700004,
+            2020011700005,
+            2020011700006,
+        ]
+        is_it_there(
+            embargo_hours,
+            now_time_embargo,
+            ids_remain,
+            ids_moved,
+            self.temp_from_path,
+            self.temp_to_path,
+            move=move,
+            log=self.log,
+            datasettype=["raw"],
+            collections=["LATISS/raw/all"],
+            desturiprefix=self.temp_dest_ingest,
+        )
+
+    def test_nothing_moves(self):
+        """
+        Nothing should move when the embargo hours falls right on
+        the oldest exposure
+        """
+        move = "False"
+        now_time_embargo = "2020-01-17 16:55:11.322700"
+        embargo_hours = 5596964.255774 / 3600.0
+        # IDs that should be moved to temp_to:
+        ids_moved = []
+        # IDs that should stay in the temp_from:
+        ids_remain = [
+            2019111300059,
+            2019111300061,
+            2020011700002,
+            2020011700003,
+            2020011700004,
+            2020011700005,
+            2020011700006,
+        ]
+        is_it_there(
+            embargo_hours,
+            now_time_embargo,
+            ids_remain,
+            ids_moved,
+            self.temp_from_path,
+            self.temp_to_path,
+            move=move,
+            log=self.log,
+            datasettype=["raw"],
+            collections=["LATISS/raw/all"],
+            desturiprefix=self.temp_dest_ingest,
+        )
 
     # test the other datatypes:
     # first goodseeingdeepcoadd
@@ -203,7 +363,7 @@ class TestMoveEmbargoArgs(unittest.TestCase):
 
     # test the other datatypes:
     # first goodseeingdeepcoadd
-    
+
     def test_raw_datatypes_should_fail(self):
         """
         Test that move_embargo_args runs for a list
@@ -229,7 +389,7 @@ class TestMoveEmbargoArgs(unittest.TestCase):
             2020011700005,
             2020011700006,
         ]
-        
+
         try:
             is_it_there(
                 embargo_hours,
@@ -249,147 +409,9 @@ class TestMoveEmbargoArgs(unittest.TestCase):
             pass
         else:
             # All assertions passed, so we raise a custom exception
-            raise AtLeastOneAssertionFailed("All assertions within is_it_there passed and they should have failed")
-  
-    def test_calexp_should_move(self):
-        """
-        Test that move_embargo_args runs for
-        the calexp datatype
-        """
-        move = "False"
-        # now_time_embargo = "now"
-        # embargo_hours =  80.0 # hours
-        now_time_embargo = "2022-11-11 03:35:12.836981"
-        #"2020-01-17 16:55:11.322700"
-        embargo_hours = 80.0  # hours
-        # IDs that should be moved to temp_to:
-        ids_moved = [
-            2022110800580,
-            2022110900673,
-            2022111000436
-        ]
-        # IDs that should stay in the temp_from:
-        ids_remain = [
-            2022110800580,
-            2022110900673,
-            2022111000436
-        ]
-        is_it_there(
-            embargo_hours,
-            now_time_embargo,
-            ids_remain,
-            ids_moved,
-            self.temp_from_path,
-            self.temp_to_path,
-            move=move,
-            log=self.log,
-            datasettype=["calexp"],
-            collections=["LATISS/runs/AUXTEL_DRP_IMAGING_2022-11A/w_2022_46/PREOPS-1616"],
-            desturiprefix=self.temp_dest_ingest,
-            # desturiprefix="tests/data/",
-        ) 
-'''
-# test the other datatypes:
-    # first goodseeingdeepcoadd
-
-    def test_calexp_should_not_move(self):
-        """
-        Test that move_embargo_args does not move
-        the calexp data that is too close to embargo
-        """
-        move = "False"
-        # now_time_embargo = "now"
-        # embargo_hours =  80.0 # hours
-        now_time_embargo = "2022-11-11 03:35:12.836981"
-        #"2020-01-17 16:55:11.322700"
-        embargo_hours = 80.0  # hours
-        # IDs that should be moved to temp_to:
-        ids_moved = []
-        # IDs that should stay in the temp_from:
-        ids_remain = [
-            2022110800580,
-            2022110900673,
-            2022111000436
-        ]
-        is_it_there(
-            embargo_hours,
-            now_time_embargo,
-            ids_remain,
-            ids_moved,
-            self.temp_from_path,
-            self.temp_to_path,
-            move=move,
-            log=self.log,
-            datasettype=["calexp"],
-            collections=["LATISS/raw/all"],
-            desturiprefix=self.temp_dest_ingest,
-            # desturiprefix="tests/data/",
-        )
-    
-
-    def test_nothing_moves(self):
-        """
-        Nothing should move when the embargo hours falls right on
-        the oldest exposure
-        """
-        move = "False"
-        now_time_embargo = "2020-01-17 16:55:11.322700"
-        embargo_hours = 5596964.255774 / 3600.0
-        # IDs that should be moved to temp_to:
-        ids_moved = []
-        # IDs that should stay in the temp_from:
-        ids_remain = [
-            2019111300059,
-            2019111300061,
-            2020011700002,
-            2020011700003,
-            2020011700004,
-            2020011700005,
-            2020011700006,
-        ]
-        is_it_there(
-            embargo_hours,
-            now_time_embargo,
-            ids_remain,
-            ids_moved,
-            self.temp_from_path,
-            self.temp_to_path,
-            move=move,
-            log=self.log,
-        )
-
-
-    def test_after_now_01(self):
-        """
-        Verify that exposures after now are not being moved
-        when the nowtime is right in the middle of the exposures
-        """
-        move = "False"
-        now_time_embargo = "2020-01-17 16:55:11.322700"
-        embargo_hours = 0.1  # hours
-        # IDs that should be moved to temp_to:
-        ids_moved = [
-            2019111300059,
-            2019111300061,
-            2020011700002,
-            2020011700003,
-        ]
-        # IDs that should stay in the temp_from:
-        ids_remain = [
-            2020011700004,
-            2020011700005,
-            2020011700006,
-        ]
-        is_it_there(
-            embargo_hours,
-            now_time_embargo,
-            ids_remain,
-            ids_moved,
-            self.temp_from_path,
-            self.temp_to_path,
-            move=move,
-            log=self.log,
-        )
+            raise AtLeastOneAssertionFailedError(
+                "All assertions within is_it_there passed and they should have failed"
+            )
 
     def test_after_now_05(self):
         """
@@ -457,6 +479,9 @@ class TestMoveEmbargoArgs(unittest.TestCase):
             self.temp_to_path,
             move=move,
             log=self.log,
+            datasettype=["raw"],
+            collections=["LATISS/raw/all"],
+            desturiprefix=self.temp_dest_ingest,
         )
 
     def test_main_copy(self):
@@ -489,6 +514,9 @@ class TestMoveEmbargoArgs(unittest.TestCase):
             self.temp_to_path,
             move=move,
             log=self.log,
+            datasettype=["raw"],
+            collections=["LATISS/raw/all"],
+            desturiprefix=self.temp_dest_ingest,
         )
 
     def test_main_move_midnight(self):
@@ -521,6 +549,9 @@ class TestMoveEmbargoArgs(unittest.TestCase):
             self.temp_to_path,
             move=move,
             log=self.log,
+            datasettype=["raw"],
+            collections=["LATISS/raw/all"],
+            desturiprefix=self.temp_dest_ingest,
         )
 
     def test_main_copy_midnight(self):
@@ -553,6 +584,9 @@ class TestMoveEmbargoArgs(unittest.TestCase):
             self.temp_to_path,
             move=move,
             log=self.log,
+            datasettype=["raw"],
+            collections=["LATISS/raw/all"],
+            desturiprefix=self.temp_dest_ingest,
         )
 
     def test_main_move_midnight_precision(self):
@@ -585,6 +619,9 @@ class TestMoveEmbargoArgs(unittest.TestCase):
             self.temp_to_path,
             move=move,
             log=self.log,
+            datasettype=["raw"],
+            collections=["LATISS/raw/all"],
+            desturiprefix=self.temp_dest_ingest,
         )
 
     def test_main_copy_midnight_precision(self):
@@ -617,8 +654,13 @@ class TestMoveEmbargoArgs(unittest.TestCase):
             self.temp_to_path,
             move=move,
             log=self.log,
+            datasettype=["raw"],
+            collections=["LATISS/raw/all"],
+            desturiprefix=self.temp_dest_ingest,
         )
+
+
 '''
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main()  
