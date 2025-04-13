03056e093a1cf7c408643852158618e9f000ba81
Kian-Tat Lim <ktl@slac.stanford.edu>
2025-04-13 02:37:41 -0700

    Create a shell script to run things.

---
+++ src/transfer_zip.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Prevent the world from accessing outputs.
+umask 027
+
+# Ensure a home directory is present for eups.
+export HOME="$TMPDIR"/"$LOGNAME"
+mkdir -p "$HOME"
+
+# Ensure SHELL is set for conda.
+export SHELL=/bin/bash
+
+source /opt/lsst/software/stack/loadLSST.sh
+setup lsst_distrib
+# RUCIO may hold multiple options, so do not quote it.
+python /opt/lsst/transfer_embargo/transfer_zip.py \
+    "$DRY_RUN" \
+    -C "$TRANSFER_CONFIG" \
+    --window "$WINDOW" \
+    -d "$DEST" \
+    $RUCIO \
+    "$FROMREPO" "$TOREPO" \
+    2>&1 |
+    if [ -d "$LOGDIR" ]; then
+	mkdir -p "$LOGDIR"/$(date +\%Y-\%m)
+        tee "$LOGDIR/$(date +\%Y-\%m)/$(date -Im)@$WINDOW".log
+    else
+	cat
+    fi
