ef9a2eacd5d1344fe1b17197b8a4db8d9e2fe513
Hsin-Fang Chiang <hchiang@lsst.org>
2025-05-21 13:59:57 -0700

    Add move_embargo_args.sh

---
+++ src/move_embargo_args.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Input environment variables:
+# SWDIR = directory where software is located
+# DRY_RUN = (optional) "--dry_run" to indicate not to affect the destination
+# NOW = (optional) "--now ISOT" argument with past datetime in isot format to consider as the current time
+# TRANSFER_CONFIG = path to transfer policy configuration
+# WINDOW = time window to scan for eligible files, previous to $NOW, as "NNmin" or "NNhr"
+# DEST = destination directory for raw zips
+# FROMREPO = source Butler repo
+# TOREPO = destination Butler repo
+
+# Prevent the world from accessing outputs.
+umask 027
+
+# Ensure a home directory is present for eups.
+export HOME="$TMPDIR"/"$LOGNAME"
+mkdir -p "$HOME"
+
+# Ensure SHELL is set for conda.
+export SHELL=/bin/bash
+
+source /opt/lsst/software/stack/loadLSST.sh
+setup lsst_distrib
+# DRY_RUN and NOW may be empty, so do not quote them.
+echo python "$SWDIR"/move_embargo_args.py \
+    $DRY_RUN \
+    $NOW \
+    -C "$TRANSFER_CONFIG" \
+    --window "$WINDOW" \
+    -d "$DEST" \
+    "$FROMREPO" "$TOREPO"
+python "$SWDIR"/move_embargo_args.py \
+    $DRY_RUN \
+    $NOW \
+    -C "$TRANSFER_CONFIG" \
+    --window "$WINDOW" \
+    -d "$DEST" \
+    "$FROMREPO" "$TOREPO" \
+    2>&1 |
+    if [ -d "$LOGDIR" ]; then
+        mkdir -p "$LOGDIR"/$(date +\%Y-\%m)/$(date -I)
+        tee "$LOGDIR/$(date +\%Y-\%m)/$(date -I)/$(date -Im)@$WINDOW".log
+    else
+        cat
+    fi
